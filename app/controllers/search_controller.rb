01211e4cf70645b65f4fef3256ee2272cec59f18
abreaux26 <angelbreaux26@gmail.com>
2021-04-16 11:57:01 -0500

    Remove commented out code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,5 @@
 class SearchController < ApplicationController
   def index
     @poems = PoemService.search(params[:author])
-
-    # @poems.each do |poem_info|
-    #   conn = Faraday.new("https://api.us-south.tone-analyzer.watson.cloud.ibm.com") do |connection|
-    #     connection.basic_auth("apikey", ENV['api_key'] )
-    #   end
-    #   response = conn.get("/instances/581b8525-9760-424b-927d-cb3e8dc2c04a/v3/tone?version=2017-09-21&text=#{poem_info[:lines].join(" ")}")
-    #   poem_data = response.body
-    #   @tones = JSON.parse(data, symbolize_names: true)
-    # end
   end
 end
